9d8f630601ebf4cc0e6f93cbcf789f0bc0da3aef
poemlover <steven95206@163.com>
2018-03-07 17:57:27 +0800

    update

---
+++ src/main/java/com/maven/crud/service/DepartmentService.java
@@ -0,0 +1,25 @@
+package com.maven.crud.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maven.crud.bean.Department;
+import com.maven.crud.dao.DepartmentMapper;
+
+@Service
+public class DepartmentService {
+
+	@Autowired
+	private DepartmentMapper departmentMapper;
+	
+	public List<Department> getDepts() {
+		
+		List<Department> list = departmentMapper.selectByExample(null);
+		
+		return list;
+	}
+
+
+}
---
+++ src/main/java/com/maven/crud/service/EmployeeService.java
@@ -0,0 +1,85 @@
+package com.maven.crud.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maven.crud.bean.Employee;
+import com.maven.crud.bean.EmployeeExample;
+import com.maven.crud.bean.EmployeeExample.Criteria;
+import com.maven.crud.dao.EmployeeMapper;
+
+@Service
+public class EmployeeService {
+
+	@Autowired
+	private EmployeeMapper employeeMapper;
+
+	/**
+	 * 查询所有员工
+	 * 
+	 * @return
+	 */
+	public List<Employee> getAll() {
+		return employeeMapper.selectByExampleWithDept(null);
+	}
+
+	public void saveEmp(Employee employee) {
+		// TODO Auto-generated method stub
+		employeeMapper.insertSelective(employee);
+	}
+	
+	/**
+	 * 检验用户名是否可用
+	 * 
+	 * @param empName
+	 * @return  true：代表当前姓名可用   fasle：不可用
+	 */
+	public boolean checkUser(String empName) {
+		EmployeeExample example = new EmployeeExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andEmpNameEqualTo(empName);
+		long count = employeeMapper.countByExample(example);
+		return count == 0;
+	}
+	
+	/**
+	 * 按照员工id查询员工
+	 * @param id
+	 * @return
+	 */
+	public Employee getEmp(Integer id) {
+		Employee employee = employeeMapper.selectByPrimaryKey(id);
+		return employee;
+	}
+	
+	/**
+	 * 员工更新
+	 * @param employee
+	 */
+	public void updateEmp(Employee employee) {
+		// TODO Auto-generated method stub
+		employeeMapper.updateByPrimaryKeySelective(employee);
+	}
+	
+	/**
+	 * 员工删除
+	 * @param id
+	 */
+	public void deleteEmp(Integer id) {
+		// TODO Auto-generated method stub
+		employeeMapper.deleteByPrimaryKey(id);
+	}
+
+	public void deleteBatch(List<Integer> ids) {
+		// TODO Auto-generated method stub
+		EmployeeExample example = new EmployeeExample();
+		Criteria criteria = example.createCriteria();
+		//delete from xxx where emp_id in(1,2,3)
+		criteria.andEmpIdIn(ids);
+		employeeMapper.deleteByExample(example);
+	}
+
+	
+}
